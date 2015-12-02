7d67b75887e5997a93ca14b29bafc42100d91bf9
tirthtp <tirth1156pb@rocketmail.com>
2015-12-02 23:05:08 +0530

    whitespaces stripping done

--- odd_one_finder.py
+++ odd_one_finder.py
@@ -9,21 +9,25 @@
 #------
 # You can do this with some different and more efficient algorithm and can use functional programming. 
 
-with open("temp1",'r') as fi :
+with open("dictionary",'r') as fi :
       list_proc = fi.read()
 new_proc=list_proc.split('\n')
+
+final_proc_list=[]
+
+for i in new_proc:
+    final_proc_list.append(i.strip())
+
+
 process_dict={}
 
-for proc in new_proc:
+for proc in final_proc_list:
       process_dict[proc]=1
 
-with open('temp2','r') as fi2 :
+with open('cur_proc','r') as fi2 :
      cur_proc=fi2.read()
-new_cur_proc=cur_proc.split('\n')
 
-for  w in new_cur_proc:
-     if w not in process_dict:
-           print w
+new_cur_proc=cur_proc.split('\n')
 
 log=open('log.txt','w')
 
