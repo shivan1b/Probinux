4c78c1ceebeda9ee58b52d7328a7148151f4b021
tirthtp <tirth1156pb@rocketmail.com>
2015-12-03 02:49:31 +0530

    Redirecting only required stuff to log.txt

--- odd_one_finder.py
+++ odd_one_finder.py
@@ -10,28 +10,32 @@
 # You can do this with some different and more efficient algorithm and can use functional programming. 
 
 with open("dictionary",'r') as fi :
-      list_proc = fi.read()
-new_proc=list_proc.split('\n')
+      list_proc = (line.rstrip() for line in fi)
+      list_proc = list(line for line in list_proc if line) 
+
+print list_proc
+'''new_proc=list_proc.split('\n')
 
 final_proc_list=[]
 
 for i in new_proc:
     final_proc_list.append(i.strip())
-
-
-process_dict={}
+print final_proc_list
+'''
+'''process_dict={}
 
 for proc in final_proc_list:
       process_dict[proc]=1
-
+'''
 with open('cur_proc','r') as fi2 :
      cur_proc=fi2.read()
 
 new_cur_proc=cur_proc.split('\n')
 
+new_cur_proc=list(set(new_cur_proc))
 log=open('log.txt','w')
 
 for  w in new_cur_proc:
-     if w not in process_dict:
+     if w not in list_proc:
            print >> log,w
 
