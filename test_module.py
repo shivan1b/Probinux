9032c944e3d9f5c08adbdb578856210d3a9e00d0
tirthtp <tirth1156pb@rocketmail.com>
2015-12-02 19:26:36 +0530

    new.py is renamed to test_module.py

---
+++ test_module.py
@@ -0,0 +1,10 @@
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+def f(x):
+    while True:
+        x*x
+if __name__ == '__main__':
+     processes = cpu_count()
+     print 'utilizing %d cores\n' % processes
+     pool = Pool(processes)
+     pool.map(f, range(processes))
